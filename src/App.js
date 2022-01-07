ef726b7ea0f7626837057d6701cf0bdaaba5a9ae
Arif Shariati <mohammad.arif.fast@hotmail.com>
2022-01-07 19:32:49 +0300

    Added basic stuff for tesing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import random from 'lodash/random';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    setNumber(random(0, 100, false));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ paddingLeft: '40%', paddingTop: '20%' }}>
+      <h4>Random Number: {number}</h4>
+      <Button onClick={handleClick} variant='contained'>Generate</Button>
+    </Container>
   );
 }
 
